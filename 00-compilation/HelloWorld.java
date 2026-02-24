1dd168bd5af8a50fbb268dcd16479b4878934906
da2inee <dain980909@naver.com>
2026-02-24 21:29:15 +0900

    자바 컴파일 과정

---
+++ 00-compilation/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * 컴파일 과정 연습용
+ * 
+ * 1. javac HelloWorld.java  → HelloWorld.class 생성
+ * 2. java HelloWorld       → 실행
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}
